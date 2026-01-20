c4b42b650d3aa295ecc6a2a4efa4cc3e6919cf43
gugaldinovs <ferramentasctv@gmail.com>
2026-01-20 16:37:36 -0300

    Update main.js

--- loja3/js/main.js
+++ loja3/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const url = 'https://pay.mecanicselecionado.site/meABG9V9815g6Ea';
+    const url = 'https://checkout.newpagamento-seguro.shop/VCCL1O8SCJ9R';
 
     const buttons = document.querySelectorAll('.pergunta-botao');
     buttons.forEach(button => {
@@ -8,4 +8,5 @@ document.addEventListener('DOMContentLoaded', () => {
             window.location.href = url + window.location.search;
         };
     });
+
 });
